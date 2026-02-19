6bde263162ed298400daae0b4a92c5dba4ca9637
07Smiley <bhushanchavan782006@eng.rizvi.edu.in>
2026-02-19 22:42:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@
 print("Simple Interest Calculator")
 
 print("Enter principal amount:")
-p = float(input())
+p = float(input().split()[-1])
 
 print("Enter rate of interest:")
-r = float(input())
+r = float(input().split()[-1])
 
 print("Enter time period (in years):")
-t = float(input())
+t = float(input().split()[-1])
 
 si = (p * r * t) / 100
 
